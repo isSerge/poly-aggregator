979668bddb58a9d9250c063b80ec14d3839fdc1d
Serge Kovbasiuk <isserge@protonmail.com>
2024-12-11 13:44:06 +0700

    Refactor lintstagedrc.json to include npm test command

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from 'node:test';
 import * as assert from 'node:assert/strict';
-import { main } from '../index.js';
+import { main } from '../main.js';
 import { setupTest } from './utils/test-utils.js';
 
 describe('Main application', () => {
