262ef762f90fa2227276fc9296eb42cc3b8b8792
Serge Kovbasiuk <isserge@protonmail.com>
2024-12-11 09:36:17 +0700

    Set up test environment variables in default.yml

--- src/test/utils/test-utils.ts
+++ src/test/utils/test-utils.ts
@@ -4,9 +4,6 @@ import { logger } from '../../logger.js';
 export function setupTest() {
   beforeEach(() => {
     logger.info('Setting up test environment');
-    // Setup test environment
-    process.env.GAMMA_API_URL = 'https://mock.gamma.api.url';
-    process.env.GEMINI_API_KEY = 'mock_gemini_api_key';
     process.env.NODE_ENV = 'test';
   });
 
