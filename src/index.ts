72498ba4f24ba3e04cf818c67b118eab240c1097
Serge Kovbasiuk <isserge@protonmail.com>
2024-12-11 16:07:24 +0700

    Simplify task scheduling by directly invoking main function and scheduling it to run every minute

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import cron from 'node-cron';
 import { main } from './main.js';
 import { config } from './config.js';
 
-// Schedule the main function
-export function scheduleTasks() {
-  cron.schedule(config.CRON_SCHEDULE, main);
-}
+// Schedule the main function to run every minute
+cron.schedule(config.CRON_SCHEDULE, main);
+
+main();
