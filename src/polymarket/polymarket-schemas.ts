3b387ae535f1110c0439fbdac78af0ef6cbf747b
Serge Kovbasiuk <isserge@protonmail.com>
2025-01-29 08:12:18 +0700

    Remove redundant fields from schema

--- src/polymarket/polymarket-schemas.ts
+++ src/polymarket/polymarket-schemas.ts
@@ -1,36 +1,8 @@
 import { z } from 'zod';
 
-const ApiTagSchema = z.object({
-  id: z.string(),
-  label: z.string(),
-  slug: z.string(),
-  forceShow: z.boolean().optional(),
-  publishedAt: z
-    .string()
-    .refine((date) => !isNaN(Date.parse(date)), {
-      message: 'Invalid date format for publishedAt',
-    })
-    .optional(),
-  createdAt: z
-    .string()
-    .refine((date) => !isNaN(Date.parse(date)), {
-      message: 'Invalid date format for createdAt',
-    })
-    .optional(),
-  updatedAt: z
-    .string()
-    .refine((date) => !isNaN(Date.parse(date)), {
-      message: 'Invalid date format for updatedAt',
-    })
-    .optional(),
-});
-
 const ApiChildMarketSchema = z.object({
   id: z.string(),
   question: z.string(),
-  conditionId: z.string(),
-  slug: z.string(),
-  resolutionSource: z.string().optional(),
   endDate: z
     .string()
     .refine((date) => !isNaN(Date.parse(date)), {
@@ -40,9 +12,6 @@ const ApiChildMarketSchema = z.object({
   startDate: z.string().refine((date) => !isNaN(Date.parse(date)), {
     message: 'Invalid date format for startDate',
   }),
-  image: z.string().optional(),
-  icon: z.string().optional(),
-  description: z.string(),
   outcomes: z.string().transform((str) => JSON.parse(str) as string[]),
   outcomePrices: z.string().transform((str) => JSON.parse(str) as string[]),
   volume: z
@@ -85,13 +54,8 @@ const ApiParentMarketSchema = z.object({
       message: 'Invalid date format for endDate',
     })
     .optional(),
-  image: z.string().url(),
-  icon: z.string().url(),
   active: z.boolean(),
   closed: z.boolean(),
-  archived: z.boolean(),
-  featured: z.boolean(),
-  restricted: z.boolean(),
   liquidity: z.number(),
   volume: z.number().optional(),
   openInterest: z.number().optional(),
@@ -102,13 +66,10 @@ const ApiParentMarketSchema = z.object({
     message: 'Invalid date format for updatedAt',
   }),
   volume24hr: z.number().optional(),
-  enableOrderBook: z.boolean(),
   markets: z.array(ApiChildMarketSchema),
-  tags: z.array(ApiTagSchema),
 });
 
 export const ApiParentMarketArraySchema = z.array(ApiParentMarketSchema);
 
 export type ApiParentMarket = z.infer<typeof ApiParentMarketSchema>;
 export type ApiChildMarket = z.infer<typeof ApiChildMarketSchema>;
-export type ApiTag = z.infer<typeof ApiTagSchema>;
